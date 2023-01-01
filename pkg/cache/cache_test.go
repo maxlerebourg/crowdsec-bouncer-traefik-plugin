9d602ef9095ccf0b722ca444be1409603c186e51
Max Lerebourg <maxlerebourg@gmail.com>
2023-01-01 19:13:21 +0100

    :bug: fix tests

--- pkg/cache/cache_test.go
+++ pkg/cache/cache_test.go
@@ -46,7 +46,7 @@ func Test_GetDecision(t *testing.T) {
 
 func Test_SetDecision(t *testing.T) {
 	client := &Client{cache: &localCache{}}
-	IPInCache := "10.0.0.10"
+	IPInCache := "10.0.0.11"
 	type args struct {
 		clientIP string
 		value    bool
@@ -76,8 +76,8 @@ func Test_SetDecision(t *testing.T) {
 }
 
 func Test_DeleteDecision(t *testing.T) {
-	IPInCache := "10.0.0.10"
-	IPNotInCache := "10.0.0.20"
+	IPInCache := "10.0.0.12"
+	IPNotInCache := "10.0.0.22"
 	client := &Client{cache: &localCache{}}
 	client.SetDecision(IPInCache, true, 10)
 	type args struct {
