62c0892d30c073f905870d3d0bedef631f811079
Max Lerebourg <maxlerebourg@gmail.com>
2024-02-10 16:50:00 +0100

    :bento: fix test

--- pkg/configuration/configuration_test.go
+++ pkg/configuration/configuration_test.go
@@ -155,7 +155,7 @@ func Test_validateParamsIPs(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if err := validateParamsIPs(tt.args.listIP, tt.args.key); (err != nil) != tt.wantErr {
+			if err := validateParamsIPs(&logger.Log{}, tt.args.listIP, tt.args.key); (err != nil) != tt.wantErr {
 				t.Errorf("validateParamsIPs() error = %v, wantErr %v", err, tt.wantErr)
 			}
 		})
