9259f5076563f4a4b6ec2299ea1c3ce00c97f566
mhx <mathieu@hanotaux.fr>
2024-03-31 17:49:00 +0200

    ✅ Fix test du to rework on cache interface

--- pkg/cache/cache_test.go
+++ pkg/cache/cache_test.go
@@ -54,22 +54,32 @@ func Test_Set(t *testing.T) {
 		value    string
 		duration int64
 	}
+
 	tests := []struct {
-		name string
-		args args
-		want string
+		name     string
+		args     args
+		want     string
+		wantErr  bool
+		valueErr string
 	}{
-		{name: "Set valid IP in local cache for 0 sec", args: args{clientIP: IPInCache, value: BannedValue, duration: 0}, want: NoBannedValue},
-		{name: "Set valid IP in local cache for 10 sec", args: args{clientIP: IPInCache, value: BannedValue, duration: 10}, want: BannedValue},
-		{name: "Set valid IP in local cache for 10 sec", args: args{clientIP: IPInCache, value: NoBannedValue, duration: 10}, want: NoBannedValue},
+		{name: "Set valid IP in local cache for 0 sec", args: args{clientIP: IPInCache, value: BannedValue, duration: 0}, want: "", wantErr: true, valueErr: CacheMiss},
+		{name: "Set valid IP in local cache for 10 sec", args: args{clientIP: IPInCache, value: BannedValue, duration: 10}, want: BannedValue, wantErr: false, valueErr: ""},
+		{name: "Set valid IP in local cache for 10 sec", args: args{clientIP: IPInCache, value: NoBannedValue, duration: 10}, want: NoBannedValue, wantErr: false, valueErr: ""},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			client.Set(tt.args.clientIP, tt.args.value, tt.args.duration)
-			got, _ := client.Get(tt.args.clientIP)
+			got, err := client.Get(tt.args.clientIP)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Set() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
 			if got != tt.want {
 				t.Errorf("Set() = %v, want %v", got, tt.want)
 				return
+			}
+			if tt.valueErr != "" && tt.valueErr != err.Error() {
+				t.Errorf("Set() err = %v, want %v", err.Error(), tt.valueErr)
 			}
 		})
 	}
@@ -83,22 +93,33 @@ func Test_Delete(t *testing.T) {
 	type args struct {
 		clientIP string
 	}
+
 	tests := []struct {
-		name string
-		args args
-		want string
+		name     string
+		args     args
+		want     string
+		wantErr  bool
+		valueErr string
 	}{
-		{name: "Delete Known valid IP", args: args{clientIP: IPInCache}, want: NoBannedValue},
-		{name: "Delete Unknown valid IP", args: args{clientIP: IPNotInCache}, want: NoBannedValue},
+		{name: "Delete Known valid IP", args: args{clientIP: IPInCache}, want: "", wantErr: true, valueErr: CacheMiss},
+		{name: "Delete Unknown valid IP", args: args{clientIP: IPNotInCache}, want: "", wantErr: true, valueErr: CacheMiss},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			client.Delete(tt.args.clientIP)
-			got, _ := client.Get(tt.args.clientIP)
+			got, err := client.Get(tt.args.clientIP)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Delete() error = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
 			if got != tt.want {
 				t.Errorf("Delete() = %v, want %v", got, tt.want)
 				return
 			}
+			if tt.valueErr != "" && tt.valueErr != err.Error() {
+				t.Errorf("Delete() err = %v, want %v", err.Error(), tt.valueErr)
+			}
+
 		})
 	}
 }
