97c8002809012de0aed928dc283f8c0b5d73a49a
max.lerebourg <max.lerebourg@monisnap.com>
2024-06-19 08:15:25 +0200

    :hammer: 174 fix classname for Turnstile

--- pkg/captcha/captcha.go
+++ pkg/captcha/captcha.go
@@ -48,7 +48,7 @@ var (
 		},
 		configuration.TurnstileProvider: {
 			js:       "https://challenges.cloudflare.com/turnstile/v0/api.js",
-			key:      "cf-captcha",
+			key:      "cf-turnstile",
 			validate: "https://challenges.cloudflare.com/turnstile/v0/siteverify",
 		},
 	}
