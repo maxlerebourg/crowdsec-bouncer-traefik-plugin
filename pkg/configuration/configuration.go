cd5712c850df9e979d61986ec72128ccd89d418f
Max Lerebourg <maxlerebourg@gmail.com>
2025-06-30 18:26:50 +0200

    🔧 Add config check

--- pkg/configuration/configuration.go
+++ pkg/configuration/configuration.go
@@ -357,6 +357,9 @@ func validateParamsRequired(config *Config) error {
 	if config.CrowdsecAppsecBodyLimit < 0 {
 		return errors.New("CrowdsecAppsecBodyLimit: cannot be less than 0")
 	}
+	if config.RemediationStatusCode < 100 || config.RemediationStatusCode >= 600  {
+		return errors.New("RemediationStatusCode: cannot be less than 100 and more than 600")
+	}
 
 	if !contains([]string{NoneMode, LiveMode, StreamMode, AloneMode, AppsecMode}, config.CrowdsecMode) {
 		return errors.New("CrowdsecMode: must be one of 'none', 'live', 'stream', 'alone' or 'appsec'")
