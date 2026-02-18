18bacb972d61d0dad99ffa23aeb46876462ccac5
Shadoweee77 <itzshadowitzshadow@gmail.com>
2026-02-18 22:43:18 +0100

    Disablie lint check for no global variables

--- version.go
+++ version.go
@@ -1,4 +1,5 @@
 package crowdsec_bouncer_traefik_plugin //nolint:revive,stylecheck
 
 // pluginVersion is updated automatically by the release workflow.
+//nolint:gochecknoglobals
 var pluginVersion = "1.5.0"
